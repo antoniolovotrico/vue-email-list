d40aa913d786cd7bc9aa6407d290049e165a082f
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-01-09 18:22:46 +0100

    add cycle for in methods to populate numArr and set it to mounted section with the api call

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,12 +1,20 @@
 let app = new Vue ({
     el: "#app",
     data: {
-       rndNum:[] 
+       numArr:[],
     },
     mounted(){
+        this.pushFunc();
         axios.get("https://flynn.boolean.careers/exercises/api/random/int")
         .then(response => {
-            console.log(response);
+            //console.log(response);
         })
+    },
+    methods:{
+        pushFunc:function() {
+            for (let i = 0; i <= 35; i++){
+                this.numArr.push(i);
+            }
+        }
     }
 })
