43dd0cfb6cb2d1fe2ff2cc125e29805e8382d187
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-01-09 20:25:38 +0100

    add comment in vue-email-list project and set new css property to display it in a better way

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,29 @@
+// Genera 10 mail tramite api e stampale in una lista
+// Usate Vue e Axios.
+// La richiesta AJAX per gli indirizzi email inviatela al seguente link:
+// https://flynn.boolean.careers/exercises/api/random/mail
+// La lista di email va stampata a schermo nella pagina html.
+
 let app = new Vue ({
     el: "#app",
     data: {
         email: [],
     },
-    mounted(){
-        for (let i=0; i<= 9; i++) {
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then(response => {
-                //console.log(response);
-                this.email.push(response.data.response);
-                console.log(this.email);  
-            })
+    methods:{
+        // this function will cycle 10 timea requesting a random email to the API
+        generateFunc:function(){
+            for (let i=0; i<= 9; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(response => {
+                    //console.log(response);
+                    this.email.push(response.data.response);
+                    console.log(this.email);  
+                })
+            }
         }
+    },
+    // set the generateFunc inside mounted section to recall it on loading page
+    mounted(){
+       this.generateFunc(); 
     }
 })
